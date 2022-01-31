c4566660bfd97e74f2fef3e8df9dbfec2d97911a
Michał Filipiak <michalfilipiak2@gmail.com>
2022-01-31 03:45:10 +0100

    cors fix

--- backend/src/main/java/com/example/Tuesday_Bookly/services/Config.java
+++ backend/src/main/java/com/example/Tuesday_Bookly/services/Config.java
@@ -58,7 +58,7 @@ public class Config implements  WebMvcConfigurer{
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("GET","POST","PUT", "DELETE");
     }
 
 
